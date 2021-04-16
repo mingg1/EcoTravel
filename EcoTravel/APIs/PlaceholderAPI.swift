be8b4ff0921210da7cb4ecd385efb18b7d2db8aa
Tuomas Bergholm <tuomas.bergholm@metropolia.fi>
2021-04-16 16:30:06 +0300

    Project structure and navigation created

---
+++ EcoTravel/APIs/PlaceholderAPI.swift
@@ -0,0 +1,8 @@
+//
+//  PlaceholderAPI.swift
+//  EcoTravel
+//
+//  Created by iosdev on 16.4.2021.
+//
+
+import Foundation
---
+++ EcoTravel/Models/PlaceholderModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlaceholderModel.swift
+//  EcoTravel
+//
+//  Created by iosdev on 16.4.2021.
+//
+
+import Foundation
